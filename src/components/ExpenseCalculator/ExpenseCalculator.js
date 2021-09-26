f9fa07cdad7737924e0d23b240e8a7b14c5d900f
andeerlb <andeerlb@gmail.com>
2021-09-26 01:03:47 -0300

    ajuste para acessibilidade

--- src/components/ExpenseCalculator/ExpenseCalculator.js
+++ src/components/ExpenseCalculator/ExpenseCalculator.js
@@ -10,7 +10,7 @@ function ExpenseCalculator() {
             <div className="expense-calculator-container">
                 <div className="expense-calculator-title">
                     <h1>Já sei, comprou e ingresso e não sabe como ir?</h1>
-                    <h4>Calma, o Rock in Rio é apenas em 2022, ainda faltam {weeks} semanas, vamos pensar em algo juntos</h4>
+                    <h2>Calma, o Rock in Rio é apenas em 2022, ainda faltam {weeks} semanas, vamos pensar em algo juntos</h2>
                 </div>
                 <hr />
                 <article>Para isso, deixei uma calculadora, com ela você pode separar os gastos desejados e obter um valor final.</article>
